ccccfa4994c5bbde055d5d78ffe71289f0e8abaf
jg993670 <jg993670@iq-206-08.iut21.u-bourgogne.fr>
2022-03-09 09:47:16 +0100

    Défi 1 : connexion au serveur VALIDE

--- ProjetRSABase/src/main/java/iutdijon/projetrsabase/defis/realisations/Defi1ConnexionAuServeur.java
+++ ProjetRSABase/src/main/java/iutdijon/projetrsabase/defis/realisations/Defi1ConnexionAuServeur.java
@@ -1,7 +1,10 @@
 package iutdijon.projetrsabase.defis.realisations;
 
 import iutdijon.projetrsabase.defis.Defi;
+import iutdijon.projetrsabase.network.Network;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * 1er défi : Connexion au serveur
@@ -11,6 +14,28 @@ public class Defi1ConnexionAuServeur extends Defi {
 
     @Override
     public void executer() throws IOException{
+        try {
+            Network.connexion();
+            
+            String messageEntrant="";
+            String messageSortant="";
+            int chiffreSortant=1;
+            while(!messageEntrant.equals("FIN"))
+            {
+                messageEntrant=Network.receiveMessage();
+                if(!messageEntrant.equals("-- Début du défi : Connexion au serveur --") && !messageEntrant.equals("OK") && !messageEntrant.equals("NOK") && !messageEntrant.equals("Defi valide") && !messageEntrant.equals("FIN")){
+                    chiffreSortant=Integer.parseInt(messageEntrant)+1;
+                    messageSortant=String.valueOf(chiffreSortant);
+                    Network.sendMessage(messageSortant);
+                }
+                
+            }
+            
+            Network.end();
+            
+        } catch (Exception ex) {
+            Logger.getLogger(Defi1ConnexionAuServeur.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
--- ProjetRSABase/src/main/java/iutdijon/projetrsabase/network/Network.java
+++ ProjetRSABase/src/main/java/iutdijon/projetrsabase/network/Network.java
@@ -7,6 +7,7 @@ package iutdijon.projetrsabase.network;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -15,36 +16,101 @@ import java.net.Socket;
  * @author Matthieu
  */
 public class Network {
-    private Socket socket; 
-    private PrintWriter pw;
-    private BufferedReader bufr;
+    
+    private static Network instance = null;
+    private Socket socket;
+    private BufferedReader fluxEntrant;
+    private PrintWriter fluxSortant;
+    private boolean connecte;
     
     /**
-     * Constructeur
+     * Constructeur de Network
+     */
+    private Network() {
+        this.connecte = false;
+    }
+    
+    /**
+     * Getter du singleton
+     * @return l'instance
+     */
+    private static Network get() {
+        if (instance == null)
+            instance = new Network();
+        
+        return instance;
+    }
+    
+    /**
+     * Création de la socket
      * @throws IOException 
      */
-    public Network() throws IOException {
-        
+    private void creationSocket() throws IOException {
+        this.socket = new Socket("127.0.0.1",1234);
     }
     
     /**
-     * Envoi d'un message
+     * Création des flux
+     * @throws IOException 
      */
-    public void sendMessage(String message) {
-       
-    }
-        
-    /**
-     * Réception d'un message
-     */
-    public String receiveMessage() throws IOException {
-        return null;
+    private void creationFlux() throws IOException {
+        InputStreamReader reader = new InputStreamReader(this.socket.getInputStream());
+        this.fluxEntrant = new BufferedReader(reader);
+        this.fluxSortant = new PrintWriter(this.socket.getOutputStream(),true);
     }
     
     /**
-     * Fin de la connexion
+     * Connexion
+     * @throws IOException 
      */
-    public void end() throws IOException {
+    public static void connexion() throws Exception {
+        get().creationSocket();
+        get().creationFlux();
         
+        /*if(recevoirLigne().equals("Saisir votre NOM"))
+            envoyerLigne(Settings.getNomUtilisateur());
+        
+        if(recevoirLigne().equals("Connexion établie")){
+            //System.out.println("test connexion");
+            get().connecte = true;
+        }*/
+    }
+    
+    /**
+     * Envoyer une ligne
+     * @param message le message à envoyer 
+     */
+    public static void sendMessage(String message) {
+        System.out.println(">" + message);
+        get().fluxSortant.println(message);
+    }
+    
+    /**
+     * Recevoir un message
+     * @return le message reçu
+     * @throws IOException 
+     */
+    public static String receiveMessage() throws Exception {
+        String message = get().fluxEntrant.readLine();
+        System.out.println("<" + message);
+        return message;
+    }
+    
+    public static void end() throws IOException
+    {
+        get().closeSocket();
+    }
+    
+    private void closeSocket() throws IOException {
+        this.socket.close();
+        System.out.println("socket deleted");
+    }
+    /**
+     * Le client est-il connecté ?
+     * @return est-ce que le client est connecté ou non.
+     */
+    public static boolean estConnecte() {
+        return get().connecte;
     }
 }
+
